c8b0fdf83ff36b5146bd11ab841c0b20f88fdb63
Shaylen <manuel.sanchez@edu.hefr.ch>
2015-06-08 21:27:29 +0200

    Tried with application.js in serializers and adapters folders, still no luck

---
+++ app/models/ember-sync-queue-model.js
@@ -0,0 +1,8 @@
+import DS from "ember-data";
+ 
+export default DS.Model.extend({
+  jobRecordType: DS.attr('string'),
+  serialized:    DS.attr(),
+  operation:     DS.attr('string'),
+  createdAt:     DS.attr('string'),
+});
